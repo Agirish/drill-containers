89bc781a832e3393b87ff186e16a504533a5cf0a
Abhishek Girish <agirish@apache.org>
2018-04-23 11:31:14 -0700

    Making sure hostnames are unique

--- scripts/launchDockerContainer.sh
+++ scripts/launchDockerContainer.sh
@@ -6,13 +6,14 @@ VERSION=$3
 
 docker pull drill/apache-${PROJECT}-${OS}:${VERSION}
 
-container_id=`eval "docker run -d --privileged -h drill drill/apache-${PROJECT}-${OS}:${VERSION}"`
+container_id=`eval "docker run -d --privileged -h ${PROJECT} drill/apache-${PROJECT}-${OS}:${VERSION}"`
 sleep 10
 container_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${container_id} )
-echo "Docker Node IP: ${container_ip}"
+echo "Docker container IP address: ${container_ip}"
 
 echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /tmp/hosts.$$
-echo -e "${container_ip}\tdrill\tdrill" >> /tmp/hosts.$$
+HOSTNAME=`echo -e "${container_ip}" | cut -d '.' -f 4`
+echo -e "${container_ip}\t${PROJECT}-${HOSTNAME}\t${PROJECT}-${HOSTNAME}" >> /tmp/hosts.$$
 
 sshpass -p "mapr" scp -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r /tmp/hosts.$$ ${container_ip}:/etc/hosts
