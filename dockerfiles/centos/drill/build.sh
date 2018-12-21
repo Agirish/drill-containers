aa317ec074d251e5a0aac0422d6e346c93319ec4
Abhishek Girish <agirish@apache.org>
2018-12-21 13:47:48 -0800

    Major restructuring of Dockerfiles

---
+++ dockerfiles/centos/drill/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT=drill
+OS=centos
+VERSION=$1
+
+if [[ ${VERSION} == "" ]]
+then
+  echo "Please enter the Drill VERSION string. For ex: 1.14.0"
+  exit -1
+fi
+
+docker build --build-arg VERSION=${VERSION} -t ${PROJECT}:${VERSION}-${OS} .
+
+docker tag ${PROJECT}:${VERSION}-${OS} agirish/${PROJECT}:${VERSION}-${OS}
+docker push agirish/${PROJECT}:${VERSION}-${OS}
+
+docker tag ${PROJECT}:${VERSION}-${OS} agirish/${PROJECT}:${VERSION}
+docker push agirish/${PROJECT}:${VERSION}
