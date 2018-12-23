b21e73d5815857ce96a850171cbfa89299c2fabb
Abhishek Girish <agirish@apache.org>
2018-12-22 18:17:45 -0800

    Adding support for both ARG and ENV VERSION strings

--- dockerfiles/ubuntu/drill/build.sh
+++ dockerfiles/ubuntu/drill/build.sh
@@ -2,18 +2,23 @@
 
 PROJECT=drill
 OS=ubuntu
-VERSION=$1
+VERSION_ARG=$1
 
-if [[ ${VERSION} == "" ]]
+if [[ ${VERSION_ARG} == "" ]]
 then
-  echo "Please enter the Drill VERSION string. For ex: 1.14.0"
-  exit -1
+  if [[ ${VERSION_ENV} == "" ]]
+  then
+    echo "Please enter the Drill VERSION string. For ex: 1.14.0"
+    exit -1
+  else
+    VERSION_ARG=VERSION_ENV
+  fi
 fi
 
-docker build --build-arg VERSION=${VERSION} -t ${PROJECT}:${VERSION}-${OS} .
+docker build --build-arg VERSION_ARG=${VERSION_ARG} -t ${PROJECT}:${VERSION_ARG}-${OS} .
 
 if [[ $? -eq 0 ]]
 then
-  docker tag ${PROJECT}:${VERSION}-${OS} agirish/${PROJECT}:${VERSION}-${OS}
-  docker push agirish/${PROJECT}:${VERSION}-${OS}
+  docker tag ${PROJECT}:${VERSION_ARG}-${OS} agirish/${PROJECT}:${VERSION_ARG}-${OS}
+  docker push agirish/${PROJECT}:${VERSION_ARG}-${OS}
 fi
--- dockerfiles/ubuntu/zookeeper/build.sh
+++ dockerfiles/ubuntu/zookeeper/build.sh
@@ -2,18 +2,23 @@
 
 PROJECT=zookeeper
 OS=ubuntu
-VERSION=$1
+VERSION_ARG=$1
 
-if [[ ${VERSION} == "" ]]
+if [[ ${VERSION_ARG} == "" ]]
 then
-  echo "Please enter the ZooKeeper VERSION string. For ex: 3.4.10"
-  exit -1
+  if [[ ${VERSION_ENV} == "" ]]
+  then
+    echo "Please enter the ZooKeeper VERSION string. For ex: 3.4.10"
+    exit -1
+  else
+    VERSION_ARG=VERSION_ENV
+  fi
 fi
 
-docker build --build-arg VERSION=${VERSION} -t ${PROJECT}:${VERSION}-${OS} .
+docker build --build-arg VERSION_ARG=${VERSION_ARG} -t ${PROJECT}:${VERSION_ARG}-${OS} .
 
 if [[ $? -eq 0 ]]
 then
-  docker tag ${PROJECT}:${VERSION}-${OS} agirish/${PROJECT}:${VERSION}-${OS}
-  docker push agirish/${PROJECT}:${VERSION}-${OS}
+  docker tag ${PROJECT}:${VERSION_ARG}-${OS} agirish/${PROJECT}:${VERSION_ARG}-${OS}
+  docker push agirish/${PROJECT}:${VERSION_ARG}-${OS}
 fi
