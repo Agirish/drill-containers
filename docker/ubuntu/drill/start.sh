b85b27d3d2bd2c5906d786af1ae78256c4012495
Abhishek Girish <agirish@apache.org>
2018-12-24 15:09:36 -0800

    Removing SSH and simplifying scripts

--- docker/ubuntu/drill/start.sh
+++ docker/ubuntu/drill/start.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-IP=$(ip addr show eth0 | grep -w inet | awk '{ print $2}' | cut -d "/" -f1)
-HOSTNAME_SUFFIX=`echo -e "${IP}" | cut -d '.' -f 4`
-hostname drill-${HOSTNAME_SUFFIX}
-
-/lib/ufw/ufw-init start
-ufw allow ssh
-ufw disable
-/lib/ufw/ufw-init stop
-
-/bin/mkdir /var/run/sshd
-/bin/chmod 0755 /var/run/sshd
-/bin/sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-nohup /usr/sbin/sshd -D &
-
 sed -i 's/localhost/zk-service/g' /opt/drill/conf/drill-override.conf
 /opt/drill/bin/drillbit.sh restart
 
