0dbb867fa22561d1a6aa50a3af931e454d88672f
Abhishek Girish <agirish@apache.org>
2018-12-23 20:20:58 -0800

    Simplifying VERSION ARG

--- dockerfiles/centos/zookeeper/build.sh
+++ dockerfiles/centos/zookeeper/build.sh
@@ -2,26 +2,21 @@
 
 PROJECT=zookeeper
 OS=centos
-VERSION_ARG=$1
+VERSION=$1
 
-if [[ ${VERSION_ARG} == "" ]]
+if [[ ${VERSION} == "" ]]
 then
-  if [[ ${VERSION_ENV} == "" ]]
-  then
-    echo "Please enter the ZooKeeper VERSION string. For ex: 3.4.10"
-    exit -1
-  else
-    VERSION_ARG=VERSION_ENV
-  fi
+  echo "Please enter the ZooKeeper VERSION string. For ex: 3.4.10"
+  exit -1
 fi
 
-docker build --build-arg VERSION_ARG=${VERSION_ARG} -t ${PROJECT}:${VERSION_ARG}-${OS} .
+docker build --build-arg VERSION=${VERSION} -t ${PROJECT}:${VERSION}-${OS} .
 
 if [[ $? -eq 0 ]]
 then
-  docker tag ${PROJECT}:${VERSION_ARG}-${OS} agirish/${PROJECT}:${VERSION_ARG}-${OS}
-  docker push agirish/${PROJECT}:${VERSION_ARG}-${OS}
+  docker tag ${PROJECT}:${VERSION}-${OS} agirish/${PROJECT}:${VERSION}-${OS}
+  docker push agirish/${PROJECT}:${VERSION}-${OS}
 
-  docker tag ${PROJECT}:${VERSION_ARG}-${OS} agirish/${PROJECT}:${VERSION_ARG}
-  docker push agirish/${PROJECT}:${VERSION_ARG}
+  docker tag ${PROJECT}:${VERSION}-${OS} agirish/${PROJECT}:${VERSION}
+  docker push agirish/${PROJECT}:${VERSION}
 fi
